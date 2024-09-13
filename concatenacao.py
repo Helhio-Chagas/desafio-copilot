d62fb5ff414249cfe1264182b59c10dbbb632a50
Hélio Chagas <helhio@gmail.com>
2024-09-13 17:11:55 -0300

    Desafio concatenacao

---
+++ concatenacao.py
@@ -0,0 +1,15 @@
+# Concatenar duas strings
+
+def concatenar_strings():
+  
+  string1 = input("Digite a primeira string: ")
+  string2 = input("Digite a segunda string: ")
+
+  # Concatenando usando o operador '+'
+  string_concatenada = string1 + string2
+
+  # Exibindo o resultado
+  print("A string concatenada é:", string_concatenada)
+
+# Chamando a função
+concatenar_strings()
